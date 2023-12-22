28fb4076c906c7ecd0c3f6407c611aae9e6b3204
programming473 <merser.max@mail.ru>
2023-12-22 11:47:08 +0300

    feat(dependency setting) reinstall project

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {router} from "./routers/main";
+import {Container} from "@mui/material";
+import {RouterProvider} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App/>
+    <Container className='container' sx={{maxWidth: '1080px !important'}}>
+       <RouterProvider router={router}/>
+    </Container>
 );

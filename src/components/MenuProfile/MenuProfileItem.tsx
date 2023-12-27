bc9413db77f26e6249f363b632277a94106a1d96
programming473 <merser.max@mail.ru>
2023-12-27 15:33:08 +0300

    feat(Menu Profile): create menu profile

---
+++ src/components/MenuProfile/MenuProfileItem.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Styles from './MenuProfile.module.scss'
+const MenuProfile = () => {
+    return (
+        <nav className={Styles.nav}>
+
+        </nav>
+    );
+};
+
+export default MenuProfile;

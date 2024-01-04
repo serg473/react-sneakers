0621811255074669ae944554f63e6c5339905f44
programming473 <merser.max@mail.ru>
2024-01-05 02:51:54 +0300

    feat(style): styling card product

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,4 +1,6 @@
+import { Box } from '@mui/material'
 import ProductItem from './ProductItem'
+
 export interface IProduct {
 	like: boolean
 	picture: string
@@ -35,13 +37,41 @@ const Product = () => {
 			title: 'Кроссовки Adidas Avryn (IG2374)',
 			price: '488.00 BYN',
 		},
+		{
+			like: false,
+			picture:
+				'https://sneakers.by/image/cache/catalog/sneakers-pics/101764/krossovki-adidas-retropy-f90-ig9988-1-695x695.jpg',
+			title: 'Кроссовки Adidas Retropy F90 (IG9988)',
+			price: '354.00 BYN',
+		},
+		{
+			like: false,
+			picture:
+				'https://sneakers.by/image/cache/catalog/sneakers-pics/101752/krossovki-adidas-adifom-ig7454-1-695x695.jpg',
+			title: 'Кроссовки Adidas AdiFOM (IG7454)',
+			price: '365.00 BYN',
+		},
+		{
+			like: false,
+			picture:
+				'https://sneakers.by/image/cache/catalog/sneakers-pics/101755/krossovki-adidas-osade-ig7330-1-695x695.jpg',
+			title: 'Кроссовки Adidas Osade (IG7330)',
+			price: '298.00 BYN',
+		},
+		{
+			like: false,
+			picture:
+				'https://sneakers.by/image/cache/catalog/sneakers-pics/101766/krossovki-adidas-avryn-ig2374-1-695x695.jpg',
+			title: 'Кроссовки Adidas Avryn (IG2374)',
+			price: '488.00 BYN',
+		},
 	]
 	return (
-		<>
+		<Box sx={{ display: 'flex', flexWrap: 'wrap', gap: '40px' }}>
 			{product.map((item, index) => (
 				<ProductItem sneaker={item} key={index} />
 			))}
-		</>
+		</Box>
 	)
 }
 
--- src/components/Product/ProductItem.tsx
+++ src/components/Product/ProductItem.tsx
@@ -1,11 +1,30 @@
+import { IoAdd } from 'react-icons/io5'
 import { IProduct } from './Product'
 import styles from './ProductItem.module.scss'
 
 const ProductItem = ({ sneaker }: { sneaker: IProduct }) => {
 	return (
 		<div className={styles.item}>
-			<img width='133' height='112' src={sneaker.picture} alt={sneaker.title} />
+			<div>
+				<img
+					width='133'
+					height='112'
+					src={sneaker.picture}
+					alt={sneaker.title}
+				/>
+			</div>
 			<h2 className={styles.item__title}>{sneaker.title}</h2>
+			<div className={styles.item__info}>
+				<div className={styles.item__price}>
+					<span>Цена:</span>
+					<strong>{sneaker.price}</strong>
+				</div>
+				<div className={styles.item__add}>
+					<button>
+						<IoAdd color='#d3d3d3' size='20px' />
+					</button>
+				</div>
+			</div>
 		</div>
 	)
 }
